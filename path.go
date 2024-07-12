9a7446e5bb811a16014603834084ed397f545c52
tb85153701 <8515370@qq.com>
2024-07-12 11:24:23 +0800

    cc-extend path

--- path.go
+++ path.go
@@ -1,5 +1,11 @@
 package gbase
 
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
+
 type pathItem interface {
 	int8 | int | int64 | string
 }
@@ -24,6 +30,63 @@ func (this Path[T]) RmHeadEnd() Path[T] {
 		this = this[0 : len(this)-1]
 	}
 	return this
+}
+
+func (this Path[T]) ToList() (list []T, err error) {
+	str := this.RmHeadEnd()
+	if str == "" {
+		return
+	}
+	strs := strings.Split(str.ToString(), ",")
+
+	switch any(list).(type) {
+	case []int64:
+		for _, s := range strs {
+			var v int64
+			v, err = strconv.ParseInt(s, 10, 64)
+			if err != nil {
+				return nil, err
+			}
+			list = append(list, any(v).(T))
+		}
+	case []int8:
+		for _, s := range strs {
+			var v int8
+			parsedInt, err := strconv.ParseInt(s, 10, 8)
+			if err != nil {
+				return nil, err
+			}
+			v = int8(parsedInt)
+			list = append(list, any(v).(T))
+		}
+	default:
+		return nil, fmt.Errorf("unsupported type")
+	}
+
+	return list, nil
+}
+
+func (this Path[T]) MustToList() (list []T) {
+	var err error
+	list, err = this.ToList()
+	if err != nil {
+		panic(fmt.Sprintf("Path.MustToList failed,err:%s", err.Error()))
+	}
+	return
+}
+
+//func (this Path[T]) FromInt64List(list []int64) Path[T] {
+//	this = Path[T](fmt.Sprintf(",%s,", util.ArrayImplode(list, ",")))
+//	return this
+//}
+//
+//func (this Path[T]) FromInt8List(list []int8) Path[T] {
+//	this = Path[T](fmt.Sprintf(",%s,", util.ArrayImplode(list, ",")))
+//	return this
+//}
+
+func (this Path[T]) Has(v any) bool {
+	return InArray(v, this.MustToList())
 }
 
 //func (this Path[T]) Vd() error {
--- utils.go
+++ utils.go
@@ -3,6 +3,7 @@ package gbase
 import (
 	"fmt"
 	"math/rand"
+	"reflect"
 	"time"
 )
 
@@ -20,4 +21,21 @@ func GetRandomString(lens int) string {
 		result = append(result, bytes[r.Intn(len(bytes))])
 	}
 	return string(result)
+}
+
+func InArray(item interface{}, arr interface{}) bool {
+	switch reflect.TypeOf(arr).Kind() {
+	case reflect.Slice:
+		s := reflect.ValueOf(arr)
+		len := s.Len()
+		for i := 0; i < len; i++ {
+			v := s.Index(i).Interface()
+			//logs.Debug(v)
+			if item == v {
+				return true
+			}
+		}
+	}
+
+	return false
 }
