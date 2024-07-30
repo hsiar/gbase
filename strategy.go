7a18c86b6319d2ba50a77e20ae8ed95d85654de6
cc <8515370@qq.com>
2024-07-30 15:02:28 +0800

    cc-mod strategy interface

--- strategy.go
+++ strategy.go
@@ -3,28 +3,36 @@ package gbase
 //通用策略模式
 
 // 策略接口
-type Strategy interface {
+//type Strategy interface {
+//
+//	//result must be ptr
+//	Process(result any, args ...any) error
+//}
+//
+//
+//
+//// 策略上下文
+//type StrategyContext struct {
+//	strategy Strategy
+//}
+//
+//func (c *StrategyContext) Set(strategy Strategy) {
+//	c.strategy = strategy
+//}
+//
+//func (c *StrategyContext) Exec(result any, args ...any) error {
+//	return c.strategy.Process(result, args...)
+//}
 
-	//result must be ptr
-	Process(result any, args ...any) error
+type IStrategy[T any] interface {
+	// Process 方法返回 T 类型的值
+	Process(args ...any) (T, error)
+	// 获取类型，用于区分不同的策略
+	GetType() int
+	// 设置及获取数据，用于外界数据传入策略逻辑
+	SetData(data ...any)
 }
 
-// 策略上下文
-type StrategyContext struct {
-	strategy Strategy
-}
-
-func (c *StrategyContext) Set(strategy Strategy) {
-	c.strategy = strategy
-}
-
-func (c *StrategyContext) Exec(result any, args ...any) error {
-	return c.strategy.Process(result, args...)
-}
-
-/*
-usage:
-  strategyContext := &StrategyContext{}
-  strategyContext.Set(&StrategyA{})
-  strategyContext.Exec(&result, arg1, arg2)
-*/
+type BaseStrategy struct{}                  //空策略
+func (s *BaseStrategy) GetType() int        { return 0 }
+func (s *BaseStrategy) SetData(data ...any) {}
