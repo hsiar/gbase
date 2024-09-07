2e167995a971a77ca77eec44813fa7e9d7d7da3c
Administrator <85153701@qq.com>
2024-09-07 21:18:07 +0800

    cc

--- model.go
+++ model.go
@@ -1,16 +1,16 @@
 package gbase
 
 type PageRequest struct {
-	Page  int `json:"page"`  //页数
-	Limit int `json:"limit"` //每页条数
+	Page  int `json:"page" query:"page"`   //页数
+	Limit int `json:"limit" query:"limit"` //每页条数
 }
 
 // 只传一个ID参数的请求
 type IdRequest struct {
-	Id int64 `json:"id" vd:"$>0;msg:'id参数缺失'"`
+	Id int64 `json:"id" query:"id" vd:"$>0;msg:'id参数缺失'"`
 }
 
 // 只传一个ID参数的请求,用于snowflake的ID
 type SnowFlakeIdRequest struct {
-	Id int64 `json:"id,string" vd:"$>0;msg:'id参数缺失'"`
+	Id int64 `json:"id,string" query:"id" vd:"$>0;msg:'id参数缺失'"`
 }
